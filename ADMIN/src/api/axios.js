e9c9e123d1f1051248ca6a6b440e6e59058cf8a2
PRITOM-0 <pritomsahawww@gmail.com>
2026-05-01 19:16:20 +0600

    URL fix

--- ADMIN/src/api/axios.js
+++ ADMIN/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-commerce-webapp-mern.onrender.com/api",
+  baseURL: "https://quick-shop-server-wdtt.onrender.com/api",
   // baseURL: "http://localhost:5000/api",
 });
 export default api;
